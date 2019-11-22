62e57ba82d4cc3997291a4656f3ab6538275b332
jumper385 <henrychen385@gmail.com>
2019-11-22 19:05:14 +0800

    hopefully this works

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('express')()
 let PORT = process.env.PORT || 8080
 
 app.get('/', (req,res) => {
-    res.json({message:'hello world')}
+    res.json({message:'hello world'})
 })
 
 app.listen(PORT, () => console.log('new user connected'))
