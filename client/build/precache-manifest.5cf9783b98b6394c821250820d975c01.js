359b02e354fa09a5a7339a6c1ae05d2c72cfea76
jumper385 <henrychen385@gmail.com>
2019-11-25 17:57:24 +0800

    new build

---
+++ client/build/precache-manifest.5cf9783b98b6394c821250820d975c01.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dffeaee0a0c51cac4193440e4d53eed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f56e2149450e6a854b80",
+    "url": "/static/js/2.ab3f08e7.chunk.js"
+  },
+  {
+    "revision": "58ed7471959b4f58f98a",
+    "url": "/static/js/main.37783ea8.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
